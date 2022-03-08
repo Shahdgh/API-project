52b4b404db1e7e051d0eacbd393ebd3c4c3a7910
Shahdgh <Shahad.Mohammed.abt.js@tuwaiq.edu.sa>
2022-03-08 13:26:06 +0300

    add models user and routes users

---
+++ middleware/checkUser.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+const { User } = require("../models/User")
+
+const checkUser = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization")
+    if (!token) return res.status(401).send("token is missing")
+
+    const decryptedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    const userId = decryptedToken.id
+
+    const userFound = await User.findById(userId)
+
+    if (!userFound) return res.status(404).send("you are not User")
+   
+    req.userId = userId
+    next()
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.message)
+  }
+}
+module.exports = checkUser
