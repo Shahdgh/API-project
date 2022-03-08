00231b0610c5d30ea45395268a20a4ef5e376f98
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-03-08 14:04:51 +0300

    add author controller

--- routes/controller/author.js
+++ routes/controller/author.js
@@ -1,1 +1,19 @@
-const authorModel 
+const authorModel = require("../../models/author");
+const addAuthor = (req, res) => {
+  const { name, age, gender, image, book } = req.body;
+  const newAuthor = new authorModel({
+    name,
+    age,
+    gender,
+    image,
+    book,
+  });
+  newAuthor.save().then((result) => {
+      res.status(200).json(result)
+  })
+  .catch((err)=>{
+      res.status(400).json(err)
+  })
+};
+
+module.exports = { addAuthor };
