6440f46276adfafcf20c00d46e9fea7acb37551f
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-03-08 15:06:43 +0300

    addll crud operations of author

--- routes/controller/author.js
+++ routes/controller/author.js
@@ -1,4 +1,17 @@
+const { findOneAndRemove } = require("../../models/author");
 const authorModel = require("../../models/author");
+
+const getAuthor = (req, res) => {
+  authorModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
 const addAuthor = (req, res) => {
   const { name, age, gender, image, book } = req.body;
   const newAuthor = new authorModel({
@@ -8,12 +21,39 @@ const addAuthor = (req, res) => {
     image,
     book,
   });
-  newAuthor.save().then((result) => {
-      res.status(200).json(result)
-  })
-  .catch((err)=>{
+  newAuthor
+    .save()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+const deleteAuthor = (req, res) => {
+  const { id } = req.params;
+  authorModel
+    .findOneAndRemove(id)
+    .exec()
+    .then((result) => {
+      res.status(200).json("Deleted");
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+const updateAuthor = (req, res) => {
+  const { name, age, image } = req.body;
+  const { id } = req.params
+  authorModel
+  .findByIdAndUpdate(id ,{$set:{name:name ,age:age,image:image}})
+  .then((result)=>{
+      res.status(200).json("updates")
+
+  }).catch((err)=>{
       res.status(400).json(err)
+
   })
 };
 
-module.exports = { addAuthor };
+module.exports = { addAuthor, getAuthor, deleteAuthor,updateAuthor };
--- routes/router/author.js
+++ routes/router/author.js
@@ -1,8 +1,20 @@
 const express = require("express")
-const {addAuthor}= require("../controller/author")
+const {
+  addAuthor,
+  getAuthor,
+  deleteAuthor,
+  updateAuthor,
+} = require("../controller/author");
 
 
 const authorRouter = express.Router()
 authorRouter.post("/addAuthor" , addAuthor)
+authorRouter.get("/getAuthor", getAuthor);
+authorRouter.delete("/delAuthor/:id", deleteAuthor);
+authorRouter.put("/updateAuthor/:id", updateAuthor);
+
+
+
+
 
 module.exports = authorRouter;
