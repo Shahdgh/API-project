a7253148e7aae87ccf799cf2ff0cbbfce234da90
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-03-08 12:18:41 +0300

    add author schema

---
+++ models/author.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const author = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  age: { type: Number },
+  image: { type: String, required: true },
+  gender: { type: String },
+  books :[{type:String}]
+});
